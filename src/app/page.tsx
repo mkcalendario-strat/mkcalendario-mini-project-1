3e780d55d2778f1ef534583115592897f7f95014
mkcalendario <i_mark.calendario@stratpoint.com>
2025-07-02 10:39:57 +0800

    feat: add technologies section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import CompaniesBar from "@/components/CompaniesBar";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
+import Techonologies from "@/components/Techonologies";
 import { Fragment } from "react";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <CompaniesBar />
       <Projects />
+      <Techonologies />
     </Fragment>
   );
 }
---
+++ src/components/Techonologies.tsx
@@ -0,0 +1,35 @@
+import { Section, SectionTitle, SectionWrapper } from "./Section";
+import Techonology from "./Technology";
+
+export default async function Techonologies() {
+  const title = "Ingredients for my Recipe";
+  const description =
+    "These are the technologies, frameworks, and tools I use to bring ideas to life that covers everything from frontend and backend development.";
+
+  const url = `${process.env.NEXT_PUBLIC_URL}/assets/data/technologies.json`;
+  const response = await fetch(url, { next: { revalidate: 3600 } });
+  const technologies: Technology[] = await response.json();
+
+  return (
+    <Section
+      id="stacks"
+      className="bg-neutral-800">
+      <SectionTitle
+        title={title}
+        tag="Tech Stack"
+        description={description}
+      />
+      <SectionWrapper className="flex flex-row flex-wrap justify-center gap-[20px]">
+        {technologies.map((technology, i) => (
+          <Techonology
+            key={i}
+            name={technology.name}
+            image={technology.image}
+            category={technology.category}
+            className="w-[calc((100%-20px)/2)] sm:w-[calc((100%-20px*3)/4)] md:w-[calc((100%-20px*6)/7)] lg:w-[calc((100%-20px*9)/10)]"
+          />
+        ))}
+      </SectionWrapper>
+    </Section>
+  );
+}
