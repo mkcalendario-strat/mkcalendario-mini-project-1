4259a3b0333912eda4d9ee34c2ffa5834e39d668
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-03 08:56:03 +0800

    feat: add navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/ui/Navbar";
+import { Fragment } from "react";
+
+export default function About() {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+}
