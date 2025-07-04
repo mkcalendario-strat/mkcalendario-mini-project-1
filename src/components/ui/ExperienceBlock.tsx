09f2ccd855d9d2302e5a7eb905953edc4024b2bb
mkcalendario-strat <i_mark.calendario@stratpoint.com>
2025-07-04 13:23:07 +0800

    style: adjust aos offset for experience block

--- src/components/ui/ExperienceBlock.tsx
+++ src/components/ui/ExperienceBlock.tsx
@@ -8,6 +8,7 @@ export function ExperienceBlock({ count, description }: ExperienceBlockProps) {
   return (
     <AOSComponent>
       <div
+        data-aos-offset="0"
         data-aos="fade-left"
         className="text-right">
         <p className="text-2xl font-black text-neutral-200 md:text-3xl lg:text-5xl">
